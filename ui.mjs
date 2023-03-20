e09d41120ff9c7f3f163a0d969a348450ef4e2c7
Anders Houbak Kristiansen <palantus@gmail.com>
2023-03-20 12:56:01 +0100

    Menu item

--- ui.mjs
+++ ui.mjs
@@ -1,1 +1,9 @@
-export let menu = []
+export let menu = [
+  {
+    title: "Mail",
+    public: true,
+    items: [
+      {title: "Send mail", path: "/mail/send", permission: "mail.send"},
+    ]
+  }
+]
